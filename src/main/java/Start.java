16a0870f94310dcc81a2eb3f62b554385f794137
Argo <dmelentev1@gmail.com>
2021-11-07 16:28:48 +0300

    Init Commit

--- src/main/java/Start.java
+++ src/main/java/Start.java
@@ -1,7 +1,7 @@
 public class Start {
     public static void main(String[] args){
-        double A = ;
-
+        double a = args[1];
+        double b = args[2];
 
 
 
@@ -9,22 +9,27 @@ public class Start {
 //Для начала необходимо описать каждую опирацию
         // Буду пока описывать свою логику и двигаться по ступеням не факт что будет правильно но тип как будет
         public void Sum(){
-
+            System.out.println (a + b);
 //        Класс отвечающий за сложение
         }
         public void Vil(){
+            System.out.println (a - b);
 //            Класс отвечающий за вычитания
         }
         public void Expound(){
+            System.out.println (a * b);
 //            Класс отвечающий за умножение
         }
         public void Droed(){
+            System.out.println (a / b);
 //            Класс отвечающий за деление
         }
         public void Step(){
+            System.out.println (a ^ b);
 //            Класс отвечающий за возведение в степень
         }
         public void Sqwert(){
+            System.out.println (sqrt( a) );
 //            Класс отвечающий за извлечение квадратного корня
         }
 /*        Идея такая пользователь вводит число А нажимает "кнопку" которая будет обращаться к классу
